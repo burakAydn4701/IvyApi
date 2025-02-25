1ab6945f59d4cdfc5e32ff83f6997ab97ea84382
Burak Aydın <burakaydin@Buraks-MacBook-Air.local>
2025-02-25 22:37:29 +0300

    comment model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,8 +1,8 @@
 class Comment < ApplicationRecord
-  belongs_to :user
   belongs_to :post, counter_cache: true
-  belongs_to :parent, class_name: 'Comment', optional: true, counter_cache: :replies_count
-  has_many :replies, class_name: 'Comment', foreign_key: :parent_id, dependent: :destroy
+  belongs_to :user
+  belongs_to :parent, class_name: "Comment", optional: true, counter_cache: :replies_count
+  has_many :replies, class_name: "Comment", foreign_key: :parent_id, dependent: :destroy
   has_many :upvotes, as: :voteable, dependent: :destroy
 
   validates :content, presence: true
