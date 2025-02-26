727827f223eb001269bf2771632ba3e9f6a03ac0
Burak Aydın <burakaydin@192.168.1.105>
2025-02-26 19:47:29 +0300

    route cleanup

--- config/routes.rb
+++ config/routes.rb
@@ -1,37 +1,26 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  # Health check route
   get "up" => "rails/health#show", as: :rails_health_check
-
-  # Defines the root path route ("/")
-  # root "posts#index"
 
   namespace :api do
     resources :communities do
       get 'posts', on: :member
       resources :posts, only: [:index]
     end
-    resources :posts, only: [:create, :update, :destroy]
-    resources :users, only: [:create]
-    post '/login', to: 'sessions#create'
-    resources :posts do
+
+    resources :posts, only: [:create, :update, :destroy] do
       resources :comments, shallow: true
       resource :upvotes, only: [:create, :destroy]
-      member do
-        post 'upvote'
-      end
     end
+
+    resources :users, only: [:create]
+    post '/login', to: 'sessions#create'
+
     resources :comments, only: [:index, :show, :create, :destroy] do
       resource :upvotes, only: [:create, :destroy]
-      member do
-        post 'upvote'
-      end
-    end
-    resources :comments, only: [:show, :destroy] do
-      resource :upvotes, only: [:create, :destroy]
-      resources :comments, only: [:create]
+      resources :comments, only: [:create], shallow: true
     end
   end
 end
