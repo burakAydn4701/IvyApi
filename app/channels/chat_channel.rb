c2087bec31cfccb711897758706a69261ffc46b0
Burak Aydın <burakaydin@192.168.1.124>
2025-03-07 21:56:07 +0300

    broadcast

--- app/channels/chat_channel.rb
+++ app/channels/chat_channel.rb
@@ -1,16 +1,54 @@
 class ChatChannel < ApplicationCable::Channel
   def subscribed
-    chat = Chat.find(params[:chat_id])
-    
-    # Check if user is part of the chat
-    if chat.sender_id == current_user.id || chat.recipient_id == current_user.id
-      stream_from "chat_#{params[:chat_id]}"
-    else
-      reject
+    if params[:chat_id].present?
+      chat = Chat.find(params[:chat_id])
+      if chat.participants.include?(current_user)
+        stream_from "chat_#{params[:chat_id]}"
+        logger.info "User #{current_user.id} subscribed to chat #{params[:chat_id]}"
+      else
+        logger.error "User #{current_user.id} attempted to subscribe to unauthorized chat #{params[:chat_id]}"
+        reject
+      end
     end
   end
 
   def unsubscribed
-    # Any cleanup needed when channel is unsubscribed
+    stop_all_streams
+    logger.info "User #{current_user.id} unsubscribed from chat"
+  end
+
+  def receive(data)
+    begin
+      chat = Chat.find(params[:chat_id])
+      return unless chat.participants.include?(current_user)
+
+      message = chat.messages.create!(
+        user: current_user,
+        body: data['message']
+      )
+
+      broadcast_message(message)
+    rescue => e
+      logger.error "Error in ChatChannel#receive: #{e.message}"
+      logger.error e.backtrace.join("\n")
+    end
+  end
+
+  private
+
+  def broadcast_message(message)
+    ActionCable.server.broadcast(
+      "chat_#{params[:chat_id]}", 
+      {
+        id: message.id,
+        body: message.body,
+        created_at: message.created_at,
+        user: {
+          id: message.user.id,
+          username: message.user.username,
+          profile_photo_url: message.user.profile_photo_url
+        }
+      }
+    )
   end
 end 
