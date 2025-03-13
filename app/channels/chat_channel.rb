b70f69ae1010eee1c177203d7a4081d7205fd86c
Burak Aydın <burakaydin@Buraks-MacBook-Air.local>
2025-03-13 17:25:07 +0300

    chat id

--- app/channels/chat_channel.rb
+++ app/channels/chat_channel.rb
@@ -1,18 +1,17 @@
 class ChatChannel < ApplicationCable::Channel
   def subscribed
     if params[:chat_id].present?
-      @chat_id = params[:chat_id]
-      @chat = Chat.find(@chat_id)
-      
-      # Check if current user is part of this chat
-      if @chat.sender_id == current_user.id || @chat.recipient_id == current_user.id
+      @chat_id = params[:chat_id]  # Store chat_id as instance variable
+      chat = Chat.find(@chat_id)
+      if chat.participants.include?(current_user)
         stream_from "chat_#{@chat_id}"
         logger.info "User #{current_user.id} subscribed to chat #{@chat_id}"
       else
-        logger.error "User #{current_user.id} attempted to subscribe to unauthorized chat #{@chat_id}"
         reject
       end
     end
+  rescue ActiveRecord::RecordNotFound
+    reject
   end
 
   def unsubscribed
@@ -22,24 +21,22 @@ class ChatChannel < ApplicationCable::Channel
 
   def receive(data)
     begin
-      @chat = Chat.find(params[:chat_id]) unless @chat
+      chat = Chat.find(@chat_id)
       
       # Check if current user is part of this chat
-      unless @chat.sender_id == current_user.id || @chat.recipient_id == current_user.id
+      unless chat.sender_id == current_user.id || chat.recipient_id == current_user.id
         logger.error "User #{current_user.id} attempted to send message to unauthorized chat #{@chat_id}"
         return
       end
 
-      message = @chat.messages.create!(
+      message = chat.messages.create!(
         user: current_user,
         body: data['message']
       )
 
-      # Log the message data for debugging
-      logger.info "Message created and broadcasting: #{message.id} - #{message.body}"
+      logger.info "Message created: #{message.id} in chat #{@chat_id}"
       
       broadcast_message(message)
-      logger.info "Message broadcast completed for ID #{message.id} in chat #{@chat_id}"
     rescue => e
       logger.error "Error in ChatChannel#receive: #{e.message}"
       logger.error e.backtrace.join("\n")
@@ -49,7 +46,6 @@ class ChatChannel < ApplicationCable::Channel
   private
 
   def broadcast_message(message)
-    # Format the message in a consistent way with MessagesController
     message_data = {
       id: message.id,
       body: message.body,
@@ -61,9 +57,7 @@ class ChatChannel < ApplicationCable::Channel
       }
     }
     
-    # Log the data being broadcast
-    logger.info "Broadcasting message data: #{message_data.inspect}"
-    
+    logger.info "Broadcasting to chat_#{@chat_id}: #{message_data.inspect}"
     ActionCable.server.broadcast("chat_#{@chat_id}", message_data)
   end
 end 
